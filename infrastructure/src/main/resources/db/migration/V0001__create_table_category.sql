b94fcd3c45db4f3140ce9878c6cf774cd7335fc2
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-06-12 20:34:37 -0300

    Created SQL files to migrate and undo category table

---
+++ infrastructure/src/main/resources/db/migration/V0001__create_table_category.sql
@@ -0,0 +1,10 @@
+CREATE TABLE category (
+    id VARCHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(4000) NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NULL,
+    CONSTRAINT PK_CATEGORY PRIMARY KEY (id)
+);
