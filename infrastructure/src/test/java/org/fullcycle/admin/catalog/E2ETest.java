b5ddef3637597a701dbfb83371b0a5a72d8ba689
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-06-18 17:12:49 -0300

    Created first E2E category test and configured testcontainers setup

---
+++ infrastructure/src/test/java/org/fullcycle/admin/catalog/E2ETest.java
@@ -0,0 +1,24 @@
+package org.fullcycle.admin.catalog;
+
+import org.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@SpringBootTest(classes = WebServerConfiguration.class)
+@ActiveProfiles("e2e-test")
+@ExtendWith(CleanupDatabaseExtension.class)
+@AutoConfigureMockMvc
+public @interface E2ETest {
+
+}
---
+++ infrastructure/src/test/java/org/fullcycle/admin/catalog/e2e/category/CategoryE2ETest.java
@@ -0,0 +1,110 @@
+package org.fullcycle.admin.catalog.e2e.category;
+
+import org.fullcycle.admin.catalog.E2ETest;
+import org.fullcycle.admin.catalog.infrastructure.category.models.CreateCategoryRequest;
+import org.fullcycle.admin.catalog.infrastructure.category.models.CreateCategoryResponse;
+import org.fullcycle.admin.catalog.infrastructure.category.models.GetCategoryResponse;
+import org.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository;
+import org.fullcycle.admin.catalog.infrastructure.configuration.json.Json;
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@E2ETest
+@Testcontainers
+class CategoryE2ETest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Container
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
+        .withDatabaseName("admin_catalog_videos")
+        .withUsername("root")
+        .withPassword("123456");
+
+    @DynamicPropertySource
+    public static void setDataSourceProperties(final DynamicPropertyRegistry registry) {
+        final var privatePort = 3306;
+        final var exposedPort = MYSQL_CONTAINER.getMappedPort(privatePort);
+        System.out.printf("MariaDB container is running on port %s\n", exposedPort);
+        registry.add("mariadb.port", () -> exposedPort);
+    }
+
+    @Test
+    void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+
+        assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var createCategoryResponse = givenACategory(expectedName, expectedDescription, expectedIsActive);
+        final var getCategoryResponse = retrieveACategoryById(createCategoryResponse.id());
+
+        assertEquals(createCategoryResponse.id(), getCategoryResponse.id());
+        assertEquals(expectedName, getCategoryResponse.name());
+        assertEquals(expectedDescription, getCategoryResponse.description());
+        assertEquals(expectedIsActive, getCategoryResponse.active());
+        assertNotNull(getCategoryResponse.createdAt());
+        assertNotNull(getCategoryResponse.updatedAt());
+        assertNull(getCategoryResponse.deletedAt());
+
+        assertEquals(1, categoryRepository.count());
+    }
+
+    private CreateCategoryResponse givenACategory(final String name, final String description, final boolean active) throws Exception {
+        final var requestBody = new CreateCategoryRequest(name, description, active);
+
+        final var request = post("/categories")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(Json.writeValueAsString(requestBody));
+
+        final var response = this.mvc.perform(request)
+            .andExpect(status().isCreated())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(header().string(HttpHeaders.LOCATION, StringContains.containsString("/categories/")))
+            .andReturn()
+            .getResponse().getContentAsString();
+
+        return Json.readValue(response, CreateCategoryResponse.class);
+    }
+
+    private GetCategoryResponse retrieveACategoryById(final String categoryId) throws Exception {
+        final var request = get("/categories/{id}", categoryId)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+            .andReturn()
+            .getResponse().getContentAsString();
+
+        return Json.readValue(response, GetCategoryResponse.class);
+    }
+
+}
