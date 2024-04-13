ed4afba907082e8b8e9b7ffccd640b27a256020c
Jonathan Henrique Medeiros <jonathan.medeiros@picpay.com>
2024-04-13 16:01:21 -0300

    fix: cleanup resources when video was deleted

--- application/src/test/java/org/fullcycle/admin/catalog/application/video/delete/DeleteVideoUseCaseTest.java
+++ application/src/test/java/org/fullcycle/admin/catalog/application/video/delete/DeleteVideoUseCaseTest.java
@@ -2,6 +2,7 @@ package org.fullcycle.admin.catalog.application.video.delete;
 
 import org.fullcycle.admin.catalog.application.UseCaseTest;
 import org.fullcycle.admin.catalog.domain.utils.IdentifierUtils;
+import org.fullcycle.admin.catalog.domain.video.MediaResourceGateway;
 import org.fullcycle.admin.catalog.domain.video.VideoGateway;
 import org.fullcycle.admin.catalog.domain.video.VideoID;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 class DeleteVideoUseCaseTest extends UseCaseTest {
@@ -22,26 +24,30 @@ class DeleteVideoUseCaseTest extends UseCaseTest {
     @Mock
     private VideoGateway videoGateway;
 
+    @Mock
+    private MediaResourceGateway mediaResourceGateway;
+
     @InjectMocks
     private DefaultDeleteVideoUseCase useCase;
 
     @Override
     protected List<Object> getMocks() {
-        return List.of(videoGateway);
+        return List.of(videoGateway, mediaResourceGateway);
     }
 
     @Test
     void givenAValidId_whenCallsDeleteVideo_shouldDeleteIt() {
         final var expectedId = VideoID.unique();
 
-        doNothing().when(videoGateway)
-            .deleteById(any());
+        doNothing().when(videoGateway).deleteById(any());
+        doNothing().when(mediaResourceGateway).clearResources(any());
 
         assertDoesNotThrow(
             () -> this.useCase.execute(expectedId.getValue())
         );
 
         verify(videoGateway).deleteById(expectedId);
+        verify(mediaResourceGateway).clearResources(expectedId);
     }
 
     @Test
@@ -49,14 +55,15 @@ class DeleteVideoUseCaseTest extends UseCaseTest {
         final var notFoundIdentifier = IdentifierUtils.unique();
         final var expectedId = VideoID.from(notFoundIdentifier);
 
-        doNothing().when(videoGateway)
-            .deleteById(any());
+        doNothing().when(videoGateway).deleteById(any());
+        doNothing().when(mediaResourceGateway).clearResources(any());
 
         assertDoesNotThrow(
             () -> this.useCase.execute(expectedId.getValue())
         );
 
         verify(videoGateway).deleteById(expectedId);
+        verify(mediaResourceGateway).clearResources(expectedId);
     }
 
     @Test
@@ -74,6 +81,26 @@ class DeleteVideoUseCaseTest extends UseCaseTest {
         );
 
         verify(videoGateway).deleteById(expectedId);
+        verify(mediaResourceGateway, never()).clearResources(any());
+    }
+
+    @Test
+    void givenAValidVideoId_whenCallsDeleteVideoAndMediaResourceGatewayThrowsUnexpectedException_shouldReceiveException() {
+        final var expectedId = VideoID.unique();
+
+        doNothing().when(videoGateway).deleteById(any());
+        doThrow(new IllegalStateException("Unexpected gateway error"))
+            .when(mediaResourceGateway)
+            .clearResources(any());
+
+        assertThrows(
+            IllegalStateException.class,
+            () -> this.useCase.execute(expectedId.getValue()),
+            "Unexpected gateway error"
+        );
+
+        verify(videoGateway).deleteById(expectedId);
+        verify(mediaResourceGateway).clearResources(any());
     }
 
 }
